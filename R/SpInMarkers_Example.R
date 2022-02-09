3c52785180c870fdcb64687258dc9ef3170da382
atuldeshpande <adeshpande@jhu.edu>
2022-02-09 11:27:43 -0500

    Adding SpInMarkers scripts

---
+++ R/SpInMarkers_Example.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+patient_id1 <- '1541'
+visiumDir <- "./VisiumData/"
+cogapsDir <- "./CoGAPS_Analysis/"
+cogapsFilePattern <- ".rds"
+
+source("./R/preprocessing.R")
+source("./R/getSpatialParameters.R")
+source("./R/getInteractingGenes.R")
+source('~/FertigLab/Visium/code/find_genes_of_interest_nonparametric_fast.R')
+source('~/FertigLab/ProductionCode/SpInMarkers/R/find_genes_of_interest_nonparametric_fast.R')
+
+pVisiumPath <- paste0(visiumDir,patient_id1)
+fullMat <- load10XExpr(pVisiumPath)
+good_gene_threshold <- 3
+goodGenes <- rownames(fullMat)[apply(fullMat,1,function(x) sum(x>0)>=good_gene_threshold)]
+fullMat <- fullMat[goodGenes,]
+spCoords <- load10XCoords(pVisiumPath)
+rownames(spCoords) <- spCoords$barcode
+pCoGAPSPath <- paste0(cogapsDir,patient_id1)
+
+
+cogapsFilePath <- dir(pCoGAPSPath,cogapsFilePattern,full.names = T)
+CoGAPS_Result <- readRDS(cogapsFilePath)
+features <- intersect(rownames(fullMat),rownames(CoGAPS_Result@featureLoadings))
+barcodes <- intersect(colnames(fullMat),rownames(CoGAPS_Result@sampleFactors))
+#ATimesP <- CoGAPS_Result@featureLoadings[features,] %*% t(CoGAPS_Result@sampleFactors[barcodes,])
+fullMat <- fullMat[features,barcodes]
+cgMat <- CoGAPS_Result@featureLoadings[features,] %*% t(CoGAPS_Result@sampleFactors[barcodes,])
+spCoords <- spCoords[barcodes,]
+spPatterns <- cbind(spCoords,CoGAPS_Result@sampleFactors[barcodes,])
+optParams <- getSpatialParameters(spPatterns)
+SpInMarkers <- getInteractingGenes(data = fullMat, reconstruction = cgMat, spatialPatterns = spPatterns, refPattern = "Pattern_8", mode = "residual")
+SpInMarkers$optParams <- optParams
+for (i in seq(1,length(SpInMarkers$interacting_genes)))
+{
+    filename <- paste0(gsub(x = cogapsFilePath,pattern = '.rds',replacement = '_SpInMarkers_'),names(SpInMarkers$interacting_genes[[i]])[1],".txt")
+    write.table(rownames(SpInMarkers$interacting_genes[[i]]),file = filename,row.names = F,col.names = F, quote = F)
+}  
+
+filename <- gsub(x = cogapsFilePath,pattern = '.rds',replacement = '_patternHotspots.txt')
+write.table((!is.na(SpInMarkers$hotspot.regions))*1,file = filename,row.names = T,col.names = T, quote = F)
+
+saveRDS(SpInMarkers, file = gsub(x = cogapsFilePath,pattern = '.rds', replacement = '_SpInMarkers.rds'))
