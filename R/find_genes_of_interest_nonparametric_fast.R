a45f564764f7ff7f911bc72dd2daff2f258e10b5
orian116 <orian0010@gmail.com>
2023-08-03 11:09:19 -0400

    All functions besides getInteractingGenes pass the BiocCheck in terms of shorter lines, length and correct number of tabs.

--- R/find_genes_of_interest_nonparametric_fast.R
+++ R/find_genes_of_interest_nonparametric_fast.R
@@ -15,9 +15,10 @@ row.dunn.test <- function(in.data,region){
     region <- factor(region)
     pattern1 <- levels(region)[2]
     pattern2 <- levels(region)[3]
-    in.ranks <- matrixStats::rowRanks(in.data,cols = !is.na(region),ties.method = "average")
+    in.ranks <- matrixStats::rowRanks(in.data,cols = !is.na(region),
+                                        ties.method = "average")
     rsub <- region[!is.na(region)]
-
+    
     N <- length(rsub)
     N1 <- sum(rsub==pattern1)
     N2 <- sum(rsub==pattern2)
@@ -39,10 +40,12 @@ row.dunn.test <- function(in.data,region){
     zP2_P1 <- (mP2-mP1)/SE12/tiesStat2
     zVals <-cbind(zP1_Int,zP2_Int,zP2_P1)
 
-    pval_1_Int <- ifelse(2*(pnorm(zP1_Int,lower.tail = TRUE))<=1,2*(pnorm(zP1_Int,lower.tail = TRUE)),1)
-    pval_2_Int <- ifelse(2*(pnorm(zP2_Int,lower.tail = TRUE))<=1,2*(pnorm(zP2_Int,lower.tail = TRUE)),1)
-    pval_2_1 <- ifelse(zP2_P1>0,2*(pnorm(zP2_P1,lower.tail = FALSE)),2*(pnorm(zP2_P1,lower.tail = TRUE)))
-
+    pval_1_Int <- ifelse(2*(pnorm(zP1_Int,lower.tail = TRUE))
+                        <=1,2*(pnorm(zP1_Int,lower.tail = TRUE)),1)
+    pval_2_Int <- ifelse(2*(pnorm(zP2_Int,lower.tail = TRUE))
+                            <=1,2*(pnorm(zP2_Int,lower.tail = TRUE)),1)
+    pval_2_1 <- ifelse(zP2_P1>0,2*(pnorm(zP2_P1,lower.tail = FALSE)),
+                        2*(pnorm(zP2_P1,lower.tail = TRUE)))
     pvals<-cbind(pval_1_Int,pval_2_Int,pval_2_1)
 return(cbind(zVals,pvals))
 }
@@ -51,8 +54,10 @@ return(cbind(zVals,pvals))
 #' find_genes_of_interest_nonparametric_fast
 #' Identify genes associated with pattern interaction.
 
-#' This function identifies genes exhibiting significantly higher values of testMat in the Interaction region of the two 
-#' patterns compared #' to regions with exclusive influence from either pattern. It uses Kruskal-Wallis test followed by
+#' This function identifies genes exhibiting significantly higher values of 
+#' testMat in the Interaction region of the two 
+#' patterns compared #' to regions with exclusive influence from either pattern.
+#' It uses Kruskal-Wallis test followed by
 #' posthoc analysis using Dunn's Test to identify the genes.
 #'
 #' @usage
@@ -63,14 +68,20 @@ return(cbind(zVals,pvals))
 #'     fdr.level = 0.05
 #' )
 #' @param    testMat A matrix of counts with cells as columns and genes as rows
-#' @param    goodGenes A vector of user specified genes expected to interact a priori. The default for this is NULL as the function can find these genes itself
-#' @param    region A data frame of the reference pattern regions that overlap with the other patterns
+#' @param    goodGenes A vector of user specified genes expected to interact 
+#' a priori. The default for this is NULL as the function can find these genes 
+#' itself
+#' @param    region A data frame of the reference pattern regions that overlap 
+#' with the other patterns
 #' @param    fdr.level False Discovery Rate. The default value is 0.05.
 #'
-#' @return a list of genes exhibiting significantly higher values of testMat in the Interaction region of the two #' patterns compared to regions with exclusive influence from either pattern.
+#' @return a list of genes exhibiting significantly higher values of testMat in 
+#' the Interaction region of the two #' patterns compared to regions with 
+#' exclusive influence from either pattern.
 
 
-find_genes_of_interest_nonparametric_fast <- function(testMat, goodGenes = NULL, region, fdr.level = 0.05)
+find_genes_of_interest_nonparametric_fast <- function(testMat, goodGenes = NULL,
+                                                    region, fdr.level = 0.05)
 {
     region <- factor(region)
     patnames <- levels(region)[which(levels(region)!="Interacting")]
@@ -80,30 +91,39 @@ find_genes_of_interest_nonparametric_fast <- function(testMat, goodGenes = NULL,
         subset_goodGenes <- intersect(rownames(testMat),goodGenes)
         testMat <- testMat[subset_goodGenes,]
         }
-    residuals.kruskal <- matrixTests::row_kruskalwallis(x = as.matrix(testMat), g = region)
-    qq <- qvalue::qvalue(residuals.kruskal$pvalue,fdr.level = fdr.level, pfdr = FALSE, pi0 = 1)
+    residuals.kruskal <- matrixTests::row_kruskalwallis(x = as.matrix(testMat),
+                                                        g = region)
+    qq <- qvalue::qvalue(residuals.kruskal$pvalue,fdr.level = fdr.level,
+                            pfdr = FALSE, pi0 = 1)
     residuals.kruskal <- cbind(residuals.kruskal,p.adj = qq$qvalues)
-    ind <- rownames(residuals.kruskal[which(residuals.kruskal$p.adj<fdr.level),])
-    residuals.dunn.test <- row.dunn.test(as.matrix(testMat[ind,]),region)
-    rownames(residuals.dunn.test) <- rownames(residuals.dunn.test) <- ind
-    qq <- qvalue::qvalue(residuals.dunn.test[,4:6],fdr.level = fdr.level, pfdr = FALSE, pi0 = 1)
-    residuals.dunn.test <- cbind(residuals.dunn.test,qq$qvalues)
-    colnames(residuals.dunn.test)[7:9] <- paste0(colnames(residuals.dunn.test)[7:9],".adj")
-    interacting_over_pattern1 <- residuals.dunn.test[,"pval_1_Int"]<fdr.level
-    interacting_over_pattern2 <- residuals.dunn.test[,"pval_2_Int"]<fdr.level
-    interacting_over_both_patterns <- interacting_over_pattern1 & interacting_over_pattern2
-    not_pattern1_diff_pattern2 <- residuals.dunn.test[,"pval_2_1"]>=fdr.level
-    exclusive_interacting_over_pattern1 <- interacting_over_pattern1 & not_pattern1_diff_pattern2
-    exclusive_interacting_over_pattern2 <- interacting_over_pattern2 & not_pattern1_diff_pattern2
-    names(interacting_over_both_patterns) <- names(exclusive_interacting_over_pattern1) <- names(exclusive_interacting_over_pattern2) <- ind
-    genes_interacting <- matrix(FALSE, nrow = length(ind),ncol = 2,dimnames = list(ind,c("Gene",paste0(pattern1,' x ', pattern2))))
-    genes_interacting[,1] <- rownames(genes_interacting)
-    genes_interacting[ind[which(exclusive_interacting_over_pattern1)],2]<-paste0("vs",pattern1)
-    genes_interacting[ind[which(exclusive_interacting_over_pattern2)],2]<-paste0("vs",pattern2)
-    genes_interacting[ind[which(interacting_over_both_patterns)],2]<-"vsBoth"
+    ind<-rownames(residuals.kruskal[which(residuals.kruskal$p.adj<fdr.level),])
+    res.dunn <- row.dunn.test(as.matrix(testMat[ind,]),region)
+    rownames(res.dunn) <- rownames(res.dunn) <- ind
+    qq <- qvalue::qvalue(res.dunn[,4:6],fdr.level = fdr.level, 
+                            pfdr = FALSE, pi0 = 1)
+    res.dunn <- cbind(res.dunn,qq$qvalues)
+    colnames(res.dunn)[7:9] <- paste0(colnames(res.dunn)[7:9],".adj")
+    interacting_over_pattern1 <- res.dunn[,"pval_1_Int"]<fdr.level
+    interacting_over_pattern2 <- res.dunn[,"pval_2_Int"]<fdr.level
+    interacting_over_both_patterns <- interacting_over_pattern1 & 
+        interacting_over_pattern2
+    not_pattern1_diff_pattern2 <- res.dunn[,"pval_2_1"]>=fdr.level
+    exc_pattern1 <- interacting_over_pattern1 & 
+        not_pattern1_diff_pattern2
+    exc_pattern2 <- interacting_over_pattern2 & 
+        not_pattern1_diff_pattern2
+    names(interacting_over_both_patterns)<-names(exc_pattern1)<-
+        names(exc_pattern2) <- ind
+    inter_genes<-matrix(FALSE,nrow=length(ind),ncol=2,
+                        dimnames=list(ind,c("Gene",
+                                            paste0(pattern1,' x ', pattern2))))
+    inter_genes[,1] <- rownames(inter_genes)
+    inter_genes[ind[which(exc_pattern1)],2]<-paste0("vs",pattern1)
+    inter_genes[ind[which(exc_pattern2)],2]<-paste0("vs",pattern2)
+    inter_genes[ind[which(interacting_over_both_patterns)],2]<-"vsBoth"
     colnames(residuals.kruskal) <- paste0("KW.",colnames(residuals.kruskal))
-    colnames(residuals.dunn.test) <- paste0("Dunn.",colnames(residuals.dunn.test))
-    genes_interacting <- cbind(genes_interacting,residuals.kruskal[ind,],residuals.dunn.test)
-    genes_interacting <- genes_interacting[genes_interacting[,2]!="FALSE",]
-    return(list(genes_interacting))
+    colnames(res.dunn) <- paste0("Dunn.",colnames(res.dunn))
+    inter_genes <- cbind(inter_genes,residuals.kruskal[ind,],res.dunn)
+    inter_genes <- inter_genes[inter_genes[,2]!="FALSE",]
+    return(list(inter_genes))
 }
--- R/getSpatialParameters.R
+++ R/getSpatialParameters.R
@@ -9,12 +9,21 @@
 ## author: Atul Deshpande
 ## email: adeshpande@jhu.edu
 
-find_kernel_outliers_for_sensitivity <- function(pattern, locs, pattern_threshold = 0.15, sigma = 10, kernelthreshold = 2, method = "Pattern_Threshold", outlier = "positive")
+find_kernel_outliers_for_sensitivity <- function(pattern,locs,
+                                                    pattern_threshold=0.15,
+                                                sigma = 10,kernelthreshold = 2,
+                                                method = "Pattern_Threshold",
+                                                outlier = "positive")
 {
-    allwin <- spatstat.geom::owin(xrange = c(min(locs$x),max(locs$x)), yrange = c(min(locs$y),max(locs$y)))
-    X <-spatstat.geom::ppp(x = locs$x, y = locs$y, window = allwin, marks = pattern)
-    Kact <- spatstat.explore::Smooth(X, at = "points", sigma = sigma, leaveoneout = TRUE)
-    Karr <- vapply(seq(1,100), function(i) {Xr <- X; spatstat.geom::marks(Xr) <- spatstat.geom::marks(X)[pracma::randperm(seq_len(length(spatstat.geom::marks(X))))]; temp <- spatstat.explore::Smooth(Xr, at = "points", sigma = sigma, leaveoneout = TRUE); return(temp)}, numeric(length(Kact)))
+    allwin<-spatstat.geom::owin(xrange = c(min(locs$x),max(locs$x)),
+                                    yrange=c(min(locs$y),max(locs$y)))
+    X<-spatstat.geom::ppp(x=locs$x,y=locs$y,window=allwin,marks=pattern)
+    Kact<-spatstat.explore::Smooth(X,at ="points",sigma=sigma,leaveoneout=TRUE)
+    Karr<-vapply(seq(1,100), function(i){Xr<-X;
+    spatstat.geom::marks(Xr)<-spatstat.geom::marks(X)[pracma::randperm(
+        seq_len(length(spatstat.geom::marks(X))))];
+    temp<-spatstat.explore::Smooth(Xr,at="points",sigma=sigma,leaveoneout=TRUE);
+        return(temp)}, numeric(length(Kact)))
     Kvec <- unlist(Karr)
     mKvec <- mean(Kvec)
     sKvec <- sd(Kvec)
@@ -22,41 +31,66 @@ find_kernel_outliers_for_sensitivity <- function(pattern, locs, pattern_threshol
     return(Kact)
 }
 
-getOptimalSigmaThresh <- function(pattern, locs, sigmaVec, threshVec){
+getOptimalSigmaThresh <- function(pattern, locs, sigVec, threshVec){
     visium.dist <- as.matrix(dist(locs))
     visium.dist.inv <-1/visium.dist
     diag(visium.dist.inv) <- 0
-    allwin <- spatstat.geom::owin(xrange = c(min(locs$x),max(locs$x)), yrange = c(min(locs$y),max(locs$y)))
-    X <-spatstat.geom::ppp(x = locs$x, y = locs$y, window = allwin, marks = pattern)
-    Ks <- vapply(sigmaVec,function(i) spatstat.explore::Smooth(X, at = "points", sigma = i, leaveoneout = TRUE), numeric(X$n))
-    mor_1 <- vapply(seq_len(length(sigmaVec)), function(i) unlist(ape::Moran.I(spatstat.geom::marks(X)-Ks[,i], visium.dist.inv)),numeric(4))
-    sigmaOpt1_ind <- which.min(abs(unlist(mor_1[1,])-unlist(mor_1[2,])))
-    if (sigmaOpt1_ind>1&&sigmaOpt1_ind<length(sigmaVec)){
-        smallSigmaVec <- seq(sigmaVec[sigmaOpt1_ind-1],sigmaVec[sigmaOpt1_ind+1],(sigmaVec[sigmaOpt1_ind+1] - sigmaVec[sigmaOpt1_ind-1])/10)
-    }else if (sigmaOpt1_ind==1){
-        smallSigmaVec <- seq(sigmaVec[sigmaOpt1_ind],sigmaVec[sigmaOpt1_ind+1],(sigmaVec[sigmaOpt1_ind+1] - sigmaVec[sigmaOpt1_ind])/10)
+    allwin<-spatstat.geom::owin(xrange=c(min(locs$x),max(locs$x)),
+                                    yrange=c(min(locs$y),max(locs$y)))
+    X<-spatstat.geom::ppp(x=locs$x,y=locs$y,window=allwin,marks=pattern)
+    Ks<-vapply(sigVec,function(i) spatstat.explore::Smooth(X,at="points",
+                                                            sigma=i,
+                                                            leaveoneout=TRUE),
+                                                            numeric(X$n))
+    mor_1<-vapply(seq_len(length(sigVec)),function(i) unlist(ape::Moran.I(
+        spatstat.geom::marks(X)-Ks[,i], visium.dist.inv)),numeric(4))
+    sigOpt1_ind <- which.min(abs(unlist(mor_1[1,])-unlist(mor_1[2,])))
+    if (sigOpt1_ind>1&&sigOpt1_ind<length(sigVec)){
+        smallsigVec<-seq(sigVec[sigOpt1_ind-1],sigVec[sigOpt1_ind+1],
+                            (sigVec[sigOpt1_ind+1]-sigVec[sigOpt1_ind-1])/10)
+    }else if (sigOpt1_ind==1){
+        smallsigVec <- seq(sigVec[sigOpt1_ind],sigVec[sigOpt1_ind+1],
+                            (sigVec[sigOpt1_ind+1] - sigVec[sigOpt1_ind])/10)
     }else{
-        smallSigmaVec <- seq(sigmaVec[sigmaOpt1_ind-1],sigmaVec[sigmaOpt1_ind],(sigmaVec[sigmaOpt1_ind] - sigmaVec[sigmaOpt1_ind-1])/10)
+        smallsigVec <- seq(sigVec[sigOpt1_ind-1],sigVec[sigOpt1_ind],
+                            (sigVec[sigOpt1_ind] - sigVec[sigOpt1_ind-1])/10)
     }
-    smallKs <- vapply(smallSigmaVec,function(i) spatstat.explore::Smooth(X, at = "points", sigma = i, leaveoneout = TRUE), numeric(X$n))
-    smallmor_2 <- vapply(seq_len(length(smallSigmaVec)), function(i) unlist(ape::Moran.I(spatstat.geom::marks(X)-smallKs[,i], visium.dist.inv)), numeric(4))
-    sigmaOpt1_ind <- which.min(abs(unlist(smallmor_2[1,])-unlist(smallmor_2[2,])))
-    Kact2 <- find_kernel_outliers_for_sensitivity(pattern = pattern, locs = locs, sigma = smallSigmaVec[sigmaOpt1_ind], method = "Kernel2", kernelthreshold = 0, outlier = "positive")
-    inds2<- (kronecker(matrix(1,1,length(threshVec)),Kact2)>threshVec)*1
-    mor_2_ind <- vapply(seq_len(length(threshVec)), function(j){visium.dist.inv_1 <- visium.dist.inv; visium.dist.inv_1[inds2[,j]==0] <- 0; unlist( ape::Moran.I(spatstat.geom::marks(X)-smallKs[,sigmaOpt1_ind],visium.dist.inv_1))}, numeric(4))
-    threshOpt1_ind <- which.min(abs(unlist(mor_2_ind[1,])-unlist(mor_2_ind[2,])))
-    return(data.frame(sigmaOpt = smallSigmaVec[sigmaOpt1_ind], threshOpt = threshVec[threshOpt1_ind]))
+    smallKs<-vapply(smallsigVec,function(i) spatstat.explore::Smooth(
+            X,at="points", sigma = i, leaveoneout = TRUE), numeric(X$n))
+    smallmor_2<-vapply(seq_len(length(smallsigVec)), function(i) unlist(
+            ape::Moran.I(spatstat.geom::marks(X)-smallKs[,i],visium.dist.inv)),
+            numeric(4))
+    sigOpt1_ind <- which.min(abs(unlist(smallmor_2[1,])-unlist(smallmor_2[2,])))
+    Kact2<-find_kernel_outliers_for_sensitivity(pattern=pattern,locs=locs,
+                                                sigma=smallsigVec[sigOpt1_ind],
+                                                method="Kernel2",
+                                                kernelthreshold=0,
+                                                outlier="positive")
+    inds2<-(kronecker(matrix(1,1,length(threshVec)),Kact2)>threshVec)*1
+    mor_2_ind<-vapply(seq_len(length(threshVec)),function(j){
+        visium.dist.inv_1<- visium.dist.inv;visium.dist.inv_1[inds2[,j]==0]<-0;
+        unlist(ape::Moran.I(spatstat.geom::marks(X)-smallKs[,sigOpt1_ind],
+                            visium.dist.inv_1))}, numeric(4))
+    threshOpt1_ind<-which.min(abs(unlist(mor_2_ind[1,])-unlist(mor_2_ind[2,])))
+    return(data.frame(
+        sigmaOpt=smallsigVec[sigOpt1_ind],threshOpt=threshVec[threshOpt1_ind]))
 }
 #===================
 #' getSpatialParameters
 #' Calculate the Optimal Parameters for Interacting Cells
 #'
-#' This function calculates the optimal width of the gaussian distribution (sigmaOpt) as well as the outlier threshold around the set of spots (thresOpt) for each pattern from a latent feature space.
+#' This function calculates the optimal width of the gaussian distribution 
+#' (sigmaOpt) as well as the outlier threshold around the set of spots 
+#' (thresOpt) for each pattern from a latent feature space.
 #'
 #' @export
 #'
-#' @param spatialPatterns  A data frame that contains the spatial coordinates for each cell type. The column names must include 'x' and 'y' as well as a set of numbered columns named  'Pattern_1.....N'.
-#' @return a numeric matrix of sigmaOpts - the optimal width of the gaussian distribution, and the thresOpt - outlier threshold around the set of spots for each pattern
+#' @param spatialPatterns  A data frame that contains the spatial coordinates 
+#' for each cell type. The column names must include 'x' and 'y' as well as a 
+#' set of numbered columns named  'Pattern_1.....N'.
+#' @return a numeric matrix of sigmaOpts - the optimal width of the gaussian 
+#' distribution, and the thresOpt - outlier threshold around the set of spots 
+#' for each pattern
 #' @examples
 #' # Create test data
 #' cells <- c()
@@ -75,11 +109,15 @@ getOptimalSigmaThresh <- function(pattern, locs, sigmaVec, threshVec){
 
 getSpatialParameters <- function(spatialPatterns){
     good_gene_threshold <- 3;
-    sigmaRes <- max(floor(min(diff(range(spatialPatterns$x)),diff(range(spatialPatterns$y)))/250),1)
-    sigmaVec <- seq(2,40*sigmaRes,sigmaRes)
+    sigmaRes <- max(floor(min(diff(range(spatialPatterns$x)),
+                                diff(range(spatialPatterns$y)))/250),1)
+    sigVec <- seq(2,40*sigmaRes,sigmaRes)
     threshVec <- seq(1,3,0.1)
-    patternList <- colnames(spatialPatterns)[startsWith(colnames(spatialPatterns),"Pattern_")]
-    optParams <- vapply(patternList, function(i) unlist(getOptimalSigmaThresh(pattern = spatialPatterns[,i], locs = data.frame(x = spatialPatterns$x, y = spatialPatterns$y), sigmaVec, threshVec)), numeric(2))
+    patternList <- colnames(spatialPatterns)[
+        startsWith(colnames(spatialPatterns),"Pattern_")]
+    optParams<-vapply(patternList,function(i) unlist(getOptimalSigmaThresh(
+        pattern=spatialPatterns[,i],locs=data.frame(
+        x=spatialPatterns$x,y=spatialPatterns$y),sigVec,threshVec)),numeric(2))
     return(optParams)
 }
 
