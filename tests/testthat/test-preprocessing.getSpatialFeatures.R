1dadebb51ac0045b78bc9983064433d0a7aaa570
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-07-24 20:27:05 -0400

    expand tests

--- tests/testthat/test-preprocessing.getSpatialFeatures.R
+++ tests/testthat/test-preprocessing.getSpatialFeatures.R
@@ -1,14 +1,27 @@
-test_that("getSpatialFeatures can read a CoGAPS object",{
-    sf <- getSpatialFeatures("assets/cogaps.rds")
+test_that("getCogaps can read a CoGAPS object",{
+    sf <- getCogaps("assets/cogaps.rds")
     expect_equal(ncol(sf), 3)
 })
 
-test_that("getSpatialFeatures can read a BayesTME object",{
-    sf <- getSpatialFeatures("assets/btme.h5ad", method = "BayesTME")
+test_that("getBtme can read a BayesTME object",{
+    sf <- getBtme("assets/btme.h5ad")
     expect_equal(ncol(sf), 3)
 })
 
-test_that("getSpatialFeatures can read a Seurat object",{
-    #to be defined
+test_that("getSpatialFeatures fails with unsupported method",{
+    expect_error(getSpatialFeatures("assets/cogaps.rds", method = "unsupported"))
+})
 
+test_that("getSpatialFeatures fails with no matching feature names",{
+    expect_error(getSpatialFeatures("assets/cogaps.rds", method = "CoGAPS", featureNames = "no_match"))
+})
+
+test_that("getSpatialFeatures work with custom regex", {
+    sf <- getSpatialFeatures("assets/cogaps.rds", method = "CoGAPS", featureNames = "_1")
+    expect_equal(ncol(sf), 1)
+})
+
+test_that("getSpatialFeatures work with a feature name set", {
+    sf <- getSpatialFeatures("assets/cogaps.rds", method = "CoGAPS", featureNames = c("Pattern_1", "Pattern_2"))
+    expect_equal(ncol(sf), 2)
 })
