4c67b57c70ef108f9985afc8b23e72c9b7fcd05a
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-09-25 06:05:21 -0400

    add dense/sparse test for row.dunn.test

---
+++ tests/testthat/test-find_genes_of_interest.R
@@ -0,0 +1,13 @@
+test_that('row dunn test works with both sparse and dense matrices',{
+    dense <- matrix(rnorm(100), ncol = 10)
+    sparse <- Matrix::Matrix(dense, sparse = TRUE)
+
+    region <- matrix(sample(c(NA, "Interacting"), 10, replace = TRUE), ncol = 1)
+    region <- factor(region)
+
+    expect_true("dgCMatrix" %in% class(sparse))
+    expect_no_error(row.dunn.test(sparse, region))
+
+    expect_true("matrix" %in% class(dense))
+    expect_no_error(row.dunn.test(dense, region))
+})
