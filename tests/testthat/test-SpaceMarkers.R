c80167271a7762f3851cfc22746cb34cc978c9da
dimalvovs <dmitrijs.lvovs@gmail.com>
2025-09-29 14:27:20 -0400

    update test

--- tests/testthat/test-SpaceMarkers.R
+++ tests/testthat/test-SpaceMarkers.R
@@ -1,34 +1,37 @@
+hdpath <- "./assets/hdvisium/binned_outputs/square_016um/"
+hdfeatures <- "./assets/hdvisium/hdvisium_cellular_compositions.csv"
+sdpath <- "./assets/visium/"
+sdfeatures <- "./assets/visium/visium_cellular_compositions.csv"
 
 test_that("Directed Visium HD throws no error", {
     expect_no_error(
-        sm <- SpaceMarkers(features='./assets/hdvisium/hdvisium_cellular_compositions.csv',
-                    data='./assets/hdvisium/binned_outputs/square_016um/', cpus=4)
+        sm <- SpaceMarkers(features = hdfeatures, data = hdpath, cpus = 4)
     )
 })
 
 test_that("UnDirected Visium HD throws no error", {
     expect_no_error(
-        sm <- SpaceMarkers(features='./assets/hdvisium/hdvisium_cellular_compositions.csv',
-                    data='./assets/hdvisium/binned_outputs/square_016um/', cpus=4,
-                    directed = FALSE)
-    )
-})
-
-test_that(" Visium SD throws no error", {
-    expect_no_error(
-        suppressWarnings(
-        #1: In Smooth.ppp(X, at = "points", sigma = sigma[1], ...):
-        #Bandwidth is close to zero: nearest-neighbour interpolation performed
-            sm <- SpaceMarkers(features='./assets/visium/visium_cellular_compositions.csv',
-                         avoid_confounders=FALSE, data='./assets/visium', cpus=4)
-        )
+        sm <- SpaceMarkers(features = hdfeatures, data = hdpath,
+                           cpus = 4, directed = FALSE)
     )
 })
 
 test_that("UnDirected Visium SD throws no error", {
     expect_no_error(
-        sm <- SpaceMarkers(features='./assets/hdvisium/hdvisium_cellular_compositions.csv',
-                    data='./assets/hdvisium/binned_outputs/square_016um/', cpus=4,
-                    directed = FALSE)
+        suppressWarnings(
+        #1: In Smooth.ppp(X, at = "points", sigma = sigma[1], ...):
+        #Bandwidth is close to zero: nearest-neighbour interpolation performed
+            sm <- SpaceMarkers(features = sdfeatures, data = sdpath,
+                               cpus = 4, directed = FALSE,
+                               avoid_confounders = FALSE)
+        )
+    )
+})
+
+test_that("Directed Visium SD throws no error", {
+    expect_no_error(
+        sm <- SpaceMarkers(features = sdfeatures, data = sdpath,
+                           cpus = 4, directed = TRUE,
+                           avoid_confounders = FALSE)
     )
 })
